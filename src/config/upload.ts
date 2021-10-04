28320ec746a9a0c542bec754decbd3b8873f527c
Charlys Torres <charlysbrowntorres@gmail.com>
2021-10-04 08:03:22 -0300

    add limits for file

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -8,6 +8,51 @@ export default {
       const fileName = `${Date.now()}-${file.originalname}`;
 
       callback(null, fileName);
-    }
-  })
+    },
+  }),
+  limits: {
+    fileSize: 2 * 1024 * 1024
+  },
 }
+
+// destination: path.join(__dirname, '..', '..', 'uploads'),
+//   storage: multer.diskStorage({
+//     destination: (request, file, callback) => {
+//       callback(null, path.join(__dirname, '..', '..', 'uploads'))
+//     },
+//     filename: (request, file, callback) => {
+//       const fileName = `${Date.now()}-${file.originalname}`;
+      
+//       callback(null, fileName);
+//     },
+//   }),
+//   limits: {
+//     fileSize: 2 * 1024 * 1024
+//   },
+//   fileFilter: (request, file, callback) => {
+//     const allowedMimes = [
+//       "images/jpeg",
+//       "images/jpg",
+//       "images/png",
+//       "images/gif"
+//     ];
+
+//     if (allowedMimes.includes(file.mimetype)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Invalid file type"));
+//     }
+//   }
+
+// const storageTypes = {
+//   local: multer.diskStorage({
+//     destination: (request, file, callback) => {
+//       callback(null, path.join(__dirname, '..', '..', 'uploads'));
+//     },
+//     filename: (request, file, callback) => {
+//       const fileName = `${Date.now()}-${file.originalname}`;
+
+//       callback(null, fileName);
+//     }
+//   }),
+// };
