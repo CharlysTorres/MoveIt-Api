3d9ed3309bc503ffe5f272cd836a84820d8bf06f
Charlys Torres <charlysbrowntorres@gmail.com>
2021-11-02 18:09:59 -0300

    config port for heroku

--- src/server.ts
+++ src/server.ts
@@ -22,4 +22,4 @@ app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(errorHandler);
 
-app.listen(3333, () => {console.log("Server is running in http://localhost:3333")});
+app.listen(process.env.PORT || 3333, () => {console.log("Server is running in http://localhost:3333")});
